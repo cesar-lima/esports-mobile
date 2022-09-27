d36a93c8fbf343b116667e83a3b02edd110c1187
cesar-lima <cesarr340@gmail.com>
2022-09-26 21:51:02 -0300

    criacao de lista de cards horizontal

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,25 +1,39 @@
-import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType } from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import { styles } from "./styles";
+import { THEME } from '../../theme';
 
-export interface GameCardProps{
+export interface GameCardProps {
     id: string;
     name: string;
     ads: string;
     cover: ImageSourcePropType;
 }
 
-interface Props extends TouchableOpacityProps{
+interface Props extends TouchableOpacityProps {
     data: GameCardProps;
 }
 
 export function GameCard({ data, ...rest }: Props) {
-    return(
+    return (
         <TouchableOpacity style={styles.container} {...rest}>
-            <ImageBackground 
+            <ImageBackground
                 style={styles.cover}
                 source={data.cover}
-            />
+            >
+                <LinearGradient
+                    colors={THEME.COLORS.FOOTER}
+                    style={styles.footer}
+                >
+                    <Text style={styles.name}>
+                        {data.name}
+                    </Text>
+                    <Text>
+                        {data.ads} anúncios
+                    </Text>
+                </LinearGradient>
+            </ImageBackground>
         </TouchableOpacity>
     );
 }
