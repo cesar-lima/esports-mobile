e462c8145e8bb0efca8c15459efaf5dd6ccd78b7
cesar-lima <cesarr340@gmail.com>
2022-09-26 21:36:14 -0300

    adicao do heading e gamecard na pagina home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,29 @@
-import { View } from "react-native";
+import React from 'react';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { View, Image } from "react-native";
+import { styles } from "./styles";
+import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+
+import { GAMES } from '../../utils/games'
 
 export function Home() {
     return(
-        <View>
-            
+        <View style={styles.container}>
+            <Image 
+                source={logoImg}
+                style={styles.logo}
+            />
+
+            <Heading 
+               title="Encontre seu duo!"
+               subtitle="Selecione o game que deseja jogar..." 
+            />
+
+            <GameCard 
+                data={GAMES[0]}
+            />
         </View>
     );
 }
